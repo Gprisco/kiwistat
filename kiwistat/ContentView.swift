05a2c56bec7970dd3a8ea8121c87ea60d5985f33
Giovanni Prisco <gprisco01@gmail.com>
2020-01-10 11:41:24 +0100

    Initial Commit

---
+++ kiwistat/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  kiwistat
+//
+//  Created by Giovanni Prisco on 10/01/2020.
+//  Copyright © 2020 Giovanni Prisco. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
