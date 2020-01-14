b7e81927131b05240737c3953d57e740d4c68a65
Antonio Alfonso <antonioalfonso.work@gmail.com>
2020-01-14 12:28:58 +0100

    Development UI (not completed)

--- kiwistat/ContentView.swift
+++ kiwistat/ContentView.swift
@@ -6,14 +6,116 @@
 //  Copyright © 2020 Giovanni Prisco. All rights reserved.
 //
 
+// https://dribbble.com/shots/5951816-Flight-Ticket-Booking-App?utm_source=Pinterest_Shot&utm_campaign=jituraut&utm_content=Flight%20Ticket%20Booking%20App&utm_medium=Social_Share
 import SwiftUI
 
-struct ContentView: View {
+func width(num: CGFloat) -> CGFloat {
+    return (UIScreen.main.bounds.width / 12) * num
+}
+
+func height(num: CGFloat) -> CGFloat {
+    return (UIScreen.main.bounds.height / 12) * num
+}
+
+struct Home: View {
+    @State private var from: String = ""
+    @State private var to: String = ""
+    @State private var date: String = ""
+    @State private var traveller: String = ""
+    
     var body: some View {
-        Text("Hello, World!").onAppear(perform: {
-            retriveFavorites()
-            saveNewFavoriteFlight()
-        })
+        VStack {
+            HStack {
+                Text("WikiStat")
+                .fontWeight(.bold)
+                .font(.title)
+            }
+            HStack {
+                 VStack {
+                    TextField("From", text: $from)
+                    .padding(8)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 10)
+                        .stroke(Color.gray, lineWidth: 1)
+                    )
+                        
+                    TextField("To", text: $to)
+                    .padding(8)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 10)
+                        .stroke(Color.gray, lineWidth: 1)
+                    )
+                     
+                }
+                VStack {
+                    Button(action: { }) {
+                        Text("Inverti")
+                        
+                    }
+                    .frame(width: 50, height: 50, alignment: .center)
+                
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 30)
+                        .stroke(Color.gray, lineWidth: 1)
+                    )
+                }.padding(8)
+            }
+            
+            HStack {
+                 VStack {
+                    TextField("TravelleDater", text: $date)
+                    .padding(Edge.Set.horizontal, 8)
+                    .padding(Edge.Set.vertical, 16)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 10)
+                        .stroke(Color.gray, lineWidth: 1)
+                    )
+                     
+                }
+                VStack {
+                   TextField("Traveller", text: $traveller)
+                    .padding(Edge.Set.horizontal, 8)
+                    .padding(Edge.Set.vertical, 16)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 10)
+                        .stroke(Color.gray, lineWidth: 1)
+                    )
+                }.frame(width: width(num: 4),alignment: .bottom)
+            }
+           
+        }.padding(16)
+    }
+}
+
+struct Tickes: View {
+    var body: some View {
+        
+        Text("Tickes, World!")
+    }
+}
+
+
+struct ContentView: View {
+    @State private var selection = 1
+    
+    
+    init() {
+        UITabBar.appearance().barTintColor = UIColor.white
+    }
+    
+    var body: some View {
+        TabView {
+            Home().tabItem {
+                Image(systemName: "1.square.fill")
+                Text("First")
+            }.padding(16)
+            
+           Tickes().tabItem {
+               Image(systemName: "2.square.fill")
+               Text("Second")
+           }
+            
+       }.accentColor(.green)
     }
 }
 
---
+++ kiwistat/Views/Home.swift
@@ -0,0 +1,21 @@
+//
+//  Home.swift
+//  kiwistat
+//
+//  Created by Antonio Alfonso on 13/01/2020.
+//  Copyright © 2020 Giovanni Prisco. All rights reserved.
+//
+
+import SwiftUI
+
+//struct Home: View {
+//    var body: some View {
+//        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+//    }
+//}
+//
+//struct Home_Previews: PreviewProvider {
+//    static var previews: some View {
+//        Home()
+//    }
+//}
