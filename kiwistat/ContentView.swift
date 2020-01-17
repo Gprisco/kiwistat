1c43bfd0474e24a3e30e05d570e9d3e782f75c23
Samuel Kebis <samokebis@gmail.com>
2020-01-17 10:26:54 +0100

    removed useless code

--- kiwistat/ContentView.swift
+++ kiwistat/ContentView.swift
@@ -36,13 +36,7 @@ struct ContentView: View {
                     Text("Second")
             }
         .tag(1)
-        }.accentColor(.green).onAppear{
-            // uncomment following to save
-//            saveNewFavoriteFlight(fetchedData: <#T##FetchedData#>)
-            retrieveFavorites { flights in
-                print("__Fetched \(flights.count) flights.")
-            }
-        }
+        }.accentColor(.green)
     }
 }
 
@@ -50,22 +44,4 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
-}
-
-func retrieveFavorites(completion: @escaping ([FavoriteFlight]) -> Void) {
-    CKManager.shared.fetchRecords { (flights, error) in
-        if !error.isEmpty {
-            print(error)
-        } else {
-            print("Successfully retrived \(flights.count) favorite flights.")
-            flights.forEach{ print("__ \($0.destination), \($0.departure), \($0.link), \($0.iconName)")}
-            completion(flights)
-        }
-    }
-}
-
-func saveNewFavoriteFlight(fetchedData: FetchedData) {
-    let f = fetchedData
-    let flight = FavoriteFlight(link: f.flight.deep_link, destination: f.flight.countryTo.code, departure: f.flight.local_departure, iconName: f.weather.first?.weather.icon ?? "no icon")
-    CKManager.shared.saveRecord(favoriteFlight: flight)
 }
