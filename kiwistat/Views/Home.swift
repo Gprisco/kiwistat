f58878924a0f4ad0742e8c206fb736d6b2636469
Antonio Alfonso <antonioalfonso.work@gmail.com>
2020-01-17 09:44:45 +0100

    update home

--- kiwistat/Views/Home.swift
+++ kiwistat/Views/Home.swift
@@ -8,53 +8,121 @@
 
 import SwiftUI
 
+struct FormData {
+    var from: String
+    var to: String
+    var dateFrom: String
+    var dateTo: String
+    var travellers: Int
+}
+
 struct Home: View {
-    @State private var from: String = ""
-    @State private var to: String = ""
-    @State private var date = Date()
-    
+    @State var formData = FormData(
+        from: "",
+        to: "",
+        dateFrom: "",
+        dateTo: "",
+        travellers: 0
+    )
+    @State var showDatePicker = false
+    @State var date = Date()
+  
+  var dateFormatter: DateFormatter {
+         let formatter = DateFormatter()
+         formatter.dateStyle = .long
+         return formatter
+     }
+  
     var body: some View {
         NavigationView {
             VStack {
+                
+                HStack {
+                    Text("KiwiStat")
+                        .fontWeight(.bold)
+                        .font(.title)
+                }.padding(.bottom, 80)
+
                 HStack {
                     VStack {
-                        Section {
-                            TextField("From", text: $from)
-                                .padding(8)
-                            
-                            TextField("To", text: $to)
-                                .padding(8)
-                        }
-                    }
-                    
-                    Button(action: {
-                        let temp = self.from
-                        self.from = self.to
-                        self.to = temp
-                    }) {
-                        Text("Inverti")
+                        TextField("From", text: $formData.from)
+                            .padding(8)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(Color.gray, lineWidth: 1)
+                        )
+                        
+                        TextField("To", text: $formData.to)
+                            .padding(8)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(Color.gray, lineWidth: 1)
+                        )
                         
                     }
-                    .frame(width: 50, height: 50, alignment: .center)
-                    .padding(3)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 100, style: .circular)
-                            .stroke(lineWidth: 1.0)
-                    )
+                    
+                    VStack {
+                        Button(action: {
+                            let temp = self.formData.from
+                            self.formData.from = self.formData.to
+                            self.formData.to = temp
+                            
+                        }) {
+                            Text("Inverti")
+                        }
+                        .frame(width: 50, height: 50, alignment: .center)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 30)
+                                .stroke(Color.gray, lineWidth: 1)
+                        )
+                    }.padding(8)
+                    
+                  
                 }
-                .padding(15)
-                
-                DatePicker(selection: self.$date, in: Date()..., displayedComponents: .date, label: { Text("Departure") })
-                    .padding(8)
+              
+           
+              
+                HStack {
+                  TextField(
+                        "Date",
+                        text: $formData.dateFrom,
+                        onEditingChanged: { (editting) in
+                            self.showDatePicker = editting
+                        }
+                    ).padding(16)
+                     .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.gray, lineWidth: 1))
+
+                }
+             Spacer()
+              VStack {
+                if showDatePicker == true {
+                  DatePicker("", selection: $date, in: Date()..., displayedComponents: .date)
                     .labelsHidden()
+                  
+                  Button("Save") {
+                    self.formData.dateFrom = self.dateFormatter.string(from: self.date)
+                    self.showDatePicker = false
+                  }
+                }
+
+              }.frame(alignment: .center)
+                .padding(.bottom, 16)
+
                 
-                NavigationLink(destination: Results(tequilaHandler: Tequila(date_from: self.date, date_to: self.date, fly_from: self.from, fly_to: self.to))) {
+                NavigationLink(
+                    destination: Results(
+                        tequilaHandler: Tequila(
+                            date_from: Date(),
+                            date_to: Date(timeIntervalSinceNow: 3600 * 24 * 15),
+                            fly_from: self.formData.from,
+                            fly_to: self.formData.to
+                        )
+                    )
+                ) {
                     Text("Submit")
                 }
-                .padding(8)
-            }
-            .navigationBarTitle(Text("KiwiStat"))
-            .background(Color.clear)
+            }.padding(16)
+            
         }
     }
 }
